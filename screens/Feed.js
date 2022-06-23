1b06502e506994aac273768ed03ad4ccb9f4f1c1
patel-dhwani <77050044+patel-dhwani@users.noreply.github.com>
2022-06-23 18:38:09 +0530

    Add files via upload

--- screens/Feed.js
+++ screens/Feed.js
@@ -25,4 +25,41 @@ export default class Feed extends Component {
                     />
                 </View>
             </View>
-}
+        )    
+    }
+
+const styles = StyleSheet.create({ 
+    container: { 
+        flex: 1, 
+        backgroundColor: "black"
+    },
+    droidsafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle: {
+        flex: 0.07,
+        flexDirection: "row"
+    },
+    appIcon: {
+        flex: 0.2,
+        JustifyContent: "center", 
+        alignItems: "center"
+    },
+    iconImage: { 
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain"
+    },
+    appTitleTextContainer: {
+        flex: 0.8,
+        JustifyContent: "center"
+    },
+    appTitleText: {
+        color: "white", 
+        fontsize: RFValue(28)
+    },
+        cardContainer: { 
+            flex: 0.85
+        }
+    });
+    
